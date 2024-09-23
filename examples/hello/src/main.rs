981739b4cf942080bcb84d9e148d49f6cd0cab25
Kevin Müller <mkevin2006@gmail.com>
2024-09-23 18:28:31 +0200

    Put exported test files into ignored directory

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -5,8 +5,12 @@ use unnamed_music::{self, file_export::FileExport};
 fn main() {
     println!("Hello example");
 
+    if std::fs::read_dir("export").is_err() {
+        std::fs::create_dir("export").unwrap();
+    }
+
     let wav_export = unnamed_music::file_export::wav_export::WavExport::new(
-        std::path::PathBuf::from_str("test.txt").unwrap(),
+        std::path::PathBuf::from_str("export/test.txt").unwrap(),
     );
     let buffer = ['a' as u8; 16];
     wav_export.export(&buffer).unwrap();
