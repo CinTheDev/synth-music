d55d9c77e54bf0d1a9d2d6a71393ab14dd22d5ba
Kevin Müller <mkevin2006@gmail.com>
2024-09-23 18:20:56 +0200

    Remove test code

--- unnamed-music/src/lib.rs
+++ unnamed-music/src/lib.rs
@@ -1,16 +1,9 @@
 pub mod file_export;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(4, 2 + 2);
     }
 }
