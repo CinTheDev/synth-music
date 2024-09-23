31b4af3de9b3a13357daa5368cabbb4d522413c8
Kevin Müller <mkevin2006@gmail.com>
2024-09-23 18:24:27 +0200

    Use the file export in the example

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,7 +1,13 @@
-use unnamed_music;
+use std::str::FromStr;
+
+use unnamed_music::{self, file_export::FileExport};
 
 fn main() {
-    println!("Add 2+2:");
-    let result = unnamed_music::add(2, 2);
-    println!("Result: {}", result);
+    println!("Hello example");
+
+    let wav_export = unnamed_music::file_export::wav_export::WavExport::new(
+        std::path::PathBuf::from_str("test.txt").unwrap(),
+    );
+    let buffer = ['a' as u8; 16];
+    wav_export.export(&buffer).unwrap();
 }
