6c57e86f890e73dc590ae0365c404ded896ae9bd
Kevin Müller <mkevin2006@gmail.com>
2024-09-23 19:13:00 +0200

    Fix types for buffer writing

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -12,15 +12,17 @@ fn main() {
     let wav_export = unnamed_music::file_export::wav_export::WavExport::new(
         std::path::PathBuf::from_str("export/test.wav").unwrap(),
     );
-    let mut buffer = [0.0; 44100];
+    let mut buffer = [0_u16; 44100];
 
-    let frequency = 440;
+    let frequency = 440.0;
     let sample_rate = 44100;
+    let amplitude = 0xFFFF as f32 * 0.1;
 
     for i in 0..buffer.len() {
         let time = i as f32 / sample_rate as f32;
-        let value = (time * 2.0 * std::f32::consts::PI * frequency as f32).sin();
-        buffer[i] = value;
+        let value = (time * 2.0 * std::f32::consts::PI * frequency).sin() * amplitude;
+
+        buffer[i] = value.round() as u16;
     }
 
     wav_export.export(bytemuck::cast_slice(&buffer)).unwrap();
