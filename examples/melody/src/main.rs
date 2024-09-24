f7925e2357b6398bb116b6eaf8f31bc47fe2b9c2
Kevin Müller <mkevin2006@gmail.com>
2024-09-24 19:26:30 +0200

    Write simple example melody in example

--- examples/melody/src/main.rs
+++ examples/melody/src/main.rs
@@ -1,3 +1,45 @@
+use instrument::{note::Note, Track};
+use unnamed_music::melody::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Melody Example");
+
+    let melody_1 = melody_1();
+
+    let debug_instrument = instrument::Instrument {
+        tracks: vec![
+            melody_1,
+        ]
+    };
+
+    let first_section = Section {
+        bpm: 120,
+        instruments: vec![
+            debug_instrument,
+        ]
+    };
+
+    let piece = MusicPiece {
+        sections: vec![
+            first_section,
+        ]
+    };
+}
+
+fn melody_1() -> Track {
+    use instrument::note::{Length, Tone};
+
+    let mut track = Track::new();
+    track.note(
+        Note::new(Length::Whole)
+            .tone(Tone::DbgA)
+    );
+
+    track.note(
+        Note::new(Length::Whole)
+            .tone(Tone::DbgB)
+            .intensity(0.7)
+    );
+
+    return track;
 }
