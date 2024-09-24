5488207f0fd0db74bfc4b94d581479b208254c42
Kevin Müller <mkevin2006@gmail.com>
2024-09-24 20:29:16 +0200

    Add real tones as placeholders

--- unnamed-music/src/melody/instrument/note.rs
+++ unnamed-music/src/melody/instrument/note.rs
@@ -1,8 +1,13 @@
 
 #[derive(Clone, Copy)]
 pub enum Tone {
-    DbgA,
-    DbgB,
+    C,
+    D,
+    E,
+    F,
+    G,
+    A,
+    B,
 }
 
 #[derive(Clone, Copy)]
@@ -15,7 +20,7 @@ pub enum Length {
 }
 
 pub struct Note {
-    pub tones: Vec<Tone>,
+    pub tones: Vec<f32>,
     pub length: Length,
     pub intensity: f32,
 }
@@ -31,12 +36,29 @@ impl Note {
     }
 
     pub fn tone(mut self, tone: Tone) -> Self {
-        self.tones.push(tone);
+        let frequency = Self::get_tone_frequency(tone);
+        self.tones.push(frequency);
         self
     }
 
     pub fn intensity(mut self, intensity: f32) -> Self {
         self.intensity = intensity;
         self
+    }
+
+    fn get_tone_frequency(tone: Tone) -> f32 {
+        match tone {
+            Tone::C => Self::get_frequency_from_a4(-9),
+            Tone::D => Self::get_frequency_from_a4(-7),
+            Tone::E => Self::get_frequency_from_a4(-5),
+            Tone::F => Self::get_frequency_from_a4(-4),
+            Tone::G => Self::get_frequency_from_a4(-2),
+            Tone::A => Self::get_frequency_from_a4(0),
+            Tone::B => Self::get_frequency_from_a4(2),
+        }
+    }
+
+    fn get_frequency_from_a4(semitones: i32) -> f32 {
+        2_f32.powf(semitones as f32 / 12.0) * 440.0
     }
 }
