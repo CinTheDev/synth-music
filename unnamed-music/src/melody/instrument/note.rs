0ece57d04969570e1145eb7d751cb58d51f984d4
Kevin Müller <mkevin2006@gmail.com>
2024-09-24 19:17:19 +0200

    Implement note

--- unnamed-music/src/melody/instrument/note.rs
+++ unnamed-music/src/melody/instrument/note.rs
@@ -17,3 +17,24 @@ pub struct Note {
     length: Length,
     intensity: f32,
 }
+
+
+impl Note {
+    pub fn new(length: Length) -> Self {
+        Self {
+            tones: Vec::new(),
+            length: length,
+            intensity: 1.0,
+        }
+    }
+
+    pub fn tone(mut self, tone: Tone) -> Self {
+        self.tones.push(tone);
+        self
+    }
+
+    pub fn intensity(mut self, intensity: f32) -> Self {
+        self.intensity = intensity;
+        self
+    }
+}
