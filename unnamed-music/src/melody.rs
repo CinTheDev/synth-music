e755f91bb45b6bf1cc15e02b6191dd17e106cf45
Kevin Müller <mkevin2006@gmail.com>
2024-09-24 19:10:09 +0200

    Mark all components of melody structs as public

--- unnamed-music/src/melody.rs
+++ unnamed-music/src/melody.rs
@@ -4,13 +4,13 @@ use instrument::Instrument;
 
 // Represents the whole music which can be exported
 pub struct MusicPiece {
-    sections: Vec<Section>,
+    pub sections: Vec<Section>,
 }
 
 pub struct Section {
-    bpm: u32,
+    pub bpm: u32,
     // TODO: Key
     // TODO: Time Signature
 
-    instruments: Vec<Instrument>,
+    pub instruments: Vec<Instrument>,
 }
