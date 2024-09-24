5e01aad629bdd45f9d0021474f6ddf227d253818
Kevin Müller <mkevin2006@gmail.com>
2024-09-24 19:13:59 +0200

    Implement simple Track functionality

--- unnamed-music/src/melody/instrument.rs
+++ unnamed-music/src/melody/instrument.rs
@@ -10,3 +10,15 @@ pub struct Instrument {
 pub struct Track {
     notes: Vec<Note>,
 }
+
+impl Track {
+    pub fn new() -> Self {
+        Self {
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn note(&mut self, note: Note) {
+        self.notes.push(note);
+    }
+}
