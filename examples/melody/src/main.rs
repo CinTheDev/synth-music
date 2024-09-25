2f47af969075445078ba32707a1ea7b9dda5cf56
Kevin Müller <mkevin2006@gmail.com>
2024-09-25 20:11:37 +0200

    Create a Track in example

--- examples/melody/src/main.rs
+++ examples/melody/src/main.rs
@@ -1,5 +1,22 @@
-use unnamed_music;
+use unnamed_music::melody::prelude::*;
 
 fn main() {
-    println!("Melody Example");   
+    println!("Melody Example");
+
+    let track_1 = track_1();
+}
+
+fn track_1() -> Track {
+    use note::Tone::*;
+    let mut track = Track::new();
+
+    track.note(First);
+    track.note(Second);
+    track.note(Third);
+    track.note(Fourth);
+    track.note(Fith);
+    track.note(Sixth);
+    track.note(Seventh);
+
+    return track;
 }
