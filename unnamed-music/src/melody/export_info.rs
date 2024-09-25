378bbd278aefac09072c1e1f87834baa72dcffe5
Kevin Müller <mkevin2006@gmail.com>
2024-09-25 19:05:34 +0200

    Fill other structs as well

--- unnamed-music/src/melody/export_info.rs
+++ unnamed-music/src/melody/export_info.rs
@@ -4,12 +4,13 @@ use std::time::Duration;
 // Holds all info of a piece relevant for exporting; consists of several
 // export tracks
 pub struct ExportMusicPiece {
-
+    tracks: Vec<ExportTrack>,
 }
 
 // Contains raw tones
 pub struct ExportTrack {
-
+    // TODO: Sound generator
+    tones: Vec<Tone>,
 }
 
 // Represents a raw tone - just a frequency, duration, and intensity
