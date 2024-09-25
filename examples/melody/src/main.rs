fe61b385902945456d6fa1fa81f1ccba4d876464
Kevin Müller <mkevin2006@gmail.com>
2024-09-25 20:14:30 +0200

    Define composition in example

--- examples/melody/src/main.rs
+++ examples/melody/src/main.rs
@@ -4,6 +4,25 @@ fn main() {
     println!("Melody Example");
 
     let track_1 = track_1();
+
+    let instrument_1 = Instrument {
+        tracks: vec![track_1],
+    };
+
+    let section_1 = Section {
+        bpm: 120.0,
+        key: MusicKey::C,
+        time_signature: (4, 4),
+
+        instruments: vec![instrument_1],
+    };
+
+    let composition = Composition {
+        sections: vec![section_1],
+    };
+
+    // TODO: More complex melody
+    // TODO: Export
 }
 
 fn track_1() -> Track {
