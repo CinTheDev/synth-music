4b8b693b4411e2f69e71c516d7f30c02f2cb6e77
Kevin Müller <mkevin2006@gmail.com>
2024-09-25 19:18:27 +0200

    Define Section struct

--- unnamed-music/src/melody/composer.rs
+++ unnamed-music/src/melody/composer.rs
@@ -1,6 +1,16 @@
 
+enum MusicKey {
+    C,
+}
+
 // A helper struct to compose a piece. At the end, an ExportMusicPiece can be
 // generated from it.
 pub struct Composer {
+    sections: Vec<Section>,
+}
 
+pub struct Section {
+    pub bpm: f32,
+    pub key: MusicKey,
+    pub time_signature: (u8, u8),
 }
