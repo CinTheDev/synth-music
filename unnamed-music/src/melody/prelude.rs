69e65fffd2dea5839f853518e356c27691d84bea
Kevin Müller <mkevin2006@gmail.com>
2024-09-27 15:07:08 +0200

    Put new enums in prelude

--- unnamed-music/src/melody/prelude.rs
+++ unnamed-music/src/melody/prelude.rs
@@ -1,5 +1,5 @@
 pub use super::composer;
-pub use composer::{Composition, Section, MusicKey};
+pub use composer::{Composition, Section, MusicKey, MusicKeyBase, MusicKeyType};
 pub use composer::instrument::{Instrument, Track};
 pub use composer::instrument::note;
 
