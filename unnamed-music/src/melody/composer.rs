71b02f662e0090b36380ea31a2a5e4b2d7ccf1c9
Kevin Müller <mkevin2006@gmail.com>
2024-09-28 17:02:47 +0200

    Extract note generation into function

--- unnamed-music/src/melody/composer.rs
+++ unnamed-music/src/melody/composer.rs
@@ -1,6 +1,7 @@
 pub mod track;
 use track::Track;
-use crate::melody::export_info::*;
+use track::note::Note;
+use super::export_info::*;
 
 use std::time::Duration;
 
@@ -72,32 +73,38 @@ impl Composition {
         let mut export_track = ExportTrack::new(instrument);
 
         for note in notes {
-            let mut frequencies = Vec::new();
-
-            for tone in &note.values {
-                let base_frequency = modify_frequency(
-                    get_note_base_frequency(*tone, section.key.key_type),
-                    note.semitones_offset,
-                );
-                let keyed_frequency = transpose_from_base(base_frequency, section.key);
-                
-                frequencies.push(keyed_frequency);
-            }
-
-            let play_duration = note.get_duration(section.bpm);
-
-            export_track.tones.push(Tone {
-                frequencies,
-                play_duration,
-                tone_duration: play_duration.mul_f32(note.play_fraction),
-                intensity: note.intensity,
-                fade_in: DEFAULT_FADE_IN,
-                fade_out: DEFAULT_FADE_OUT,
-            });
+            let tone = Self::generate_tone(note, &section);
+            export_track.tones.push(tone);
         }
 
         export_section.tracks.push(export_track);
         return export_section;
+    }
+
+    fn generate_tone(note: Note, section: &Section) -> Tone {
+        let mut frequencies = Vec::new();
+
+        for tone in &note.values {
+            let base_frequency = modify_frequency(
+                get_note_base_frequency(*tone, section.key.key_type),
+                note.semitones_offset,
+            );
+            let keyed_frequency = transpose_from_base(base_frequency, section.key);
+            
+            frequencies.push(keyed_frequency);
+        }
+
+        let play_duration = note.get_duration(section.bpm);
+        let tone_duration = play_duration.mul_f32(note.play_fraction);
+
+        Tone {
+            frequencies,
+            play_duration,
+            tone_duration,
+            intensity: note.intensity,
+            fade_in: DEFAULT_FADE_IN,
+            fade_out: DEFAULT_FADE_OUT,
+        }
     }
 }
 
