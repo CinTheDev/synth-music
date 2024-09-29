50856afd60ff8bc30cfe6ce79804e3acf119714d
Kevin Müller <mkevin2006@gmail.com>
2024-09-29 14:59:34 +0200

    Update hello example export function

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,39 +1,24 @@
-use std::str::FromStr;
-
-use unnamed_music::{self, file_export::FileExport};
+use unnamed_music::melody::prelude::*;
 
 fn main() {
     println!("Hello example");
+}
+
+fn export(export_piece: ExportMusicPiece) {
+    use unnamed_music::file_export::*;
+    use wav_export::WavExport;
+    use std::path::PathBuf;
 
     if std::fs::read_dir("export").is_err() {
         std::fs::create_dir("export").unwrap();
     }
 
-    let mut buffer = vec![0_i16; 44100];
-    
-    let sample_rate = 44100;
-    let frequency = 440.0;
-    let amplitude = 0xFFFF as f32 * 0.1;
-
-    for i in 0..buffer.len() {
-        let time = i as f32 / sample_rate as f32;
-        let value = (time * 2.0 * std::f32::consts::PI * frequency).sin() * amplitude;
-
-        buffer[i] = value.round() as i16;
-    }
-
-    let wav_export = unnamed_music::file_export::wav_export::WavExport{
-        path: std::path::PathBuf::from_str("export/test.wav").unwrap(),
-        sample_rate,
-        bits_per_sample: 16,
+    let music_buffer = MusicBuffer::new(export_piece);
+    let exporter = WavExport {
+        path: PathBuf::from("export/test.wav"),
+        sample_rate: 44100,
+        ..Default::default()
     };
 
-    let byte_buffer: &[u8] = bytemuck::cast_slice(&buffer);
-    let vec_buffer = byte_buffer.to_vec();
-
-    let music_buffer = unnamed_music::file_export::MusicBuffer::new(
-        vec_buffer
-    );
-
-    wav_export.export(music_buffer).unwrap();
+    exporter.export(music_buffer).unwrap();
 }
