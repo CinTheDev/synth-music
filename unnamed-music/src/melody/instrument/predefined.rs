8e3cbee78919e1d66ff6e5b72726d106c91c7ecc
Kevin Müller <mkevin2006@gmail.com>
2024-09-30 20:15:54 +0200

    Implement structure for PredefinedInstrument

--- unnamed-music/src/melody/instrument/predefined.rs
+++ unnamed-music/src/melody/instrument/predefined.rs
@@ -1,10 +1,30 @@
 use super::Instrument;
+use super::ToneInfo;
 
 pub struct SineGenerator;
 
 pub struct TriangleGenerator;
 
 pub enum PredefinedInstrument {
-    SineGenerator,
-    TriangleGenerator,
+    SineGenerator(SineGenerator),
+    TriangleGenerator(TriangleGenerator),
+}
+
+impl PredefinedInstrument {
+    pub fn sine_generator(attributes: &SineGenerator, info: ToneInfo) -> f32 {
+        todo!();
+    }
+
+    pub fn triangle_generator(attributes: &TriangleGenerator, info: ToneInfo) -> f32 {
+        todo!();
+    }
+}
+
+impl Instrument for PredefinedInstrument {
+    fn generate_sound(&self, info: ToneInfo) -> f32 {
+        match self {
+            Self::SineGenerator(attributes) => Self::sine_generator(attributes, info),
+            Self::TriangleGenerator(attributes) => Self::triangle_generator(attributes, info),
+        }
+    }
 }
