701c5dbe31f1a77ad1a4d7e35a2d522a110ff353
Kevin Müller <mkevin2006@gmail.com>
2024-09-30 20:25:25 +0200

    Implement predefined instruments

--- unnamed-music/src/melody/instrument/predefined.rs
+++ unnamed-music/src/melody/instrument/predefined.rs
@@ -11,12 +11,20 @@ pub enum PredefinedInstrument {
 }
 
 impl PredefinedInstrument {
-    pub fn sine_generator(attributes: &SineGenerator, info: ToneInfo) -> f32 {
-        todo!();
+    pub fn sine_generator(_attributes: &SineGenerator, info: ToneInfo) -> f32 {
+        use std::f64::consts::PI;
+        
+        let frequency = info.tone.to_frequency() as f64;
+        let x = info.time.as_secs_f64() * frequency * 2.0 * PI;
+        return x.sin() as f32 * info.intensity;
     }
 
-    pub fn triangle_generator(attributes: &TriangleGenerator, info: ToneInfo) -> f32 {
-        todo!();
+    pub fn triangle_generator(_attributes: &TriangleGenerator, info: ToneInfo) -> f32 {
+        use std::f64::consts::PI;
+
+        let frequency = info.tone.to_frequency() as f64;
+        let x = info.time.as_secs_f64() * frequency * 2.0 * PI;
+        return x.sin().asin() as f32 * info.intensity;
     }
 }
 
