77bcebbec7c31cb8287f5426234b49f56c154730
Kevin Müller <mkevin2006@gmail.com>
2024-10-01 19:41:00 +0200

    Make ToneInfo generic

--- unnamed-music/src/melody/instrument.rs
+++ unnamed-music/src/melody/instrument.rs
@@ -5,12 +5,12 @@ use std::time::Duration;
 //use crate::melody::export_info::TET12ConcreteValue;
 
 #[derive(Clone, Copy)]
-pub struct ToneInfo {
-    pub tone: TET12ConcreteValue,
+pub struct ToneInfo<T> {
+    pub tone: T,
     pub time: Duration,
     pub intensity: f32,
 }
 
-pub trait Instrument {
-    fn generate_sound(&self, info: ToneInfo) -> f32;
+pub trait Instrument<T> {
+    fn generate_sound(&self, info: ToneInfo<T>) -> f32;
 }
