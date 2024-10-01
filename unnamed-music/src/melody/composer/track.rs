4fcec30792da6d90d24145b9494ea18c71f81177
Kevin Müller <mkevin2006@gmail.com>
2024-10-01 19:42:58 +0200

    Adjust and reenable track.rs

--- unnamed-music/src/melody/composer/track.rs
+++ unnamed-music/src/melody/composer/track.rs
@@ -1,20 +1,19 @@
 pub mod note;
 
-/*
-use note::Note;
-use note::scaled_value::TET12ScaledValue;
+use note::{Note, ScaledValue};
+//use note::scaled_value::TET12ScaledValue;
 use crate::melody::instrument::Instrument;
 
 #[derive(Clone)]
-pub struct Track<T: Instrument> {
-    notes: Vec<Note>,
-    instrument: T,
+pub struct Track<T: ScaledValue<T>, U: Instrument<U>> {
+    notes: Vec<Note<T>>,
+    instrument: U,
 
     current_intensity: f32,
 }
 
-impl<T: Instrument> Track<T> {
-    pub fn new(instrument: T) -> Self {
+impl<T: ScaledValue<T>, U: Instrument<U>> Track<T, U> {
+    pub fn new(instrument: U) -> Self {
         Self {
             notes: Vec::new(),
             instrument,
@@ -22,15 +21,15 @@ impl<T: Instrument> Track<T> {
         }
     }
 
-    pub fn into_parts(self) -> (Vec<Note>, T) {
+    pub fn into_parts(self) -> (Vec<Note<T>>, U) {
         (self.notes, self.instrument)
     }
 
     pub fn note(
         &mut self,
         length: note::Length,
-        value: TET12ScaledValue,
-    ) -> &mut Note {
+        value: T,
+    ) -> &mut Note<T> {
         self.notes.push(Note {
             values: vec![value],
             length,
@@ -47,8 +46,8 @@ impl<T: Instrument> Track<T> {
     pub fn notes(
         &mut self,
         length: note::Length,
-        values: Vec<TET12ScaledValue>,
-    ) -> &mut Note {
+        values: Vec<T>,
+    ) -> &mut Note<T> {
         self.notes.push(Note {
             values,
             length,
@@ -99,4 +98,3 @@ macro_rules! sequential_notes {
         )*
     };
 }
-*/
