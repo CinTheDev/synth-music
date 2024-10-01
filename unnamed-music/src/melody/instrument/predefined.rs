63790ae497730936f4b054b80cc75ebfb2f2021b
Kevin Müller <mkevin2006@gmail.com>
2024-10-01 20:35:59 +0200

    Restructure predefined.rs instruments

--- unnamed-music/src/melody/instrument/predefined.rs
+++ unnamed-music/src/melody/instrument/predefined.rs
@@ -1,14 +1,14 @@
-/*
 use super::Instrument;
 use super::ToneInfo;
 
+#[derive(Clone)]
 pub enum PredefinedInstrument {
     SineGenerator,
     TriangleGenerator,
 }
 
 impl PredefinedInstrument {
-    pub fn sine_generator(info: ToneInfo) -> f32 {
+    pub fn sine_generator(info: ToneInfo<&TET12ConcreteTone>) -> f32 {
         use std::f64::consts::PI;
         
         let frequency = info.tone.to_frequency() as f64;
@@ -16,7 +16,7 @@ impl PredefinedInstrument {
         return x.sin() as f32 * info.intensity;
     }
 
-    pub fn triangle_generator(info: ToneInfo) -> f32 {
+    pub fn triangle_generator(info: ToneInfo<&TET12ConcreteTone>) -> f32 {
         use std::f64::consts::PI;
 
         let frequency = info.tone.to_frequency() as f64;
@@ -26,14 +26,15 @@ impl PredefinedInstrument {
 }
 
 impl Instrument for PredefinedInstrument {
-    fn generate_sound(&self, info: ToneInfo) -> f32 {
+    type ConcreteValue = TET12ConcreteTone;
+
+    fn generate_sound(&self, info: ToneInfo<&Self::ConcreteValue>) -> f32 {
         match self {
             Self::SineGenerator => Self::sine_generator(info),
             Self::TriangleGenerator => Self::triangle_generator(info),
         }
     }
 }
-*/
 
 // Implementation of the 12-TET system
 use crate::melody::composer::music_key::{MusicKey, KeyType, KeyTonic};
