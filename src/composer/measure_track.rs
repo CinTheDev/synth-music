07b81381aac952dc44403ef5a98be34ad8b93c82
Kevin Müller <mkevin2006@gmail.com>
2024-10-03 13:30:05 +0200

    Fill MeasureTrack with some info

--- src/composer/measure_track.rs
+++ src/composer/measure_track.rs
@@ -1,4 +1,19 @@
+use super::note::{Note, ScaledValue, Length};
+use super::{SectionInfo, MusicTrack};
+use crate::instrument::Instrument;
+use crate::file_export::export_info::{ExportTrack, Tone};
 
-pub struct MeasureTrack {
-    
+pub struct MeasureTrack<T, U>
+where 
+    T: ScaledValue<ConcreteValue = U::ConcreteValue>,
+    U: Instrument,
+{
+    measures: Vec<Measue<T>>,
+    instrument: U,
+
+    current_intensity: f32,
+}
+
+pub struct Measue<T: ScaledValue> {
+    notes: Vec<Note<T>>,
 }
