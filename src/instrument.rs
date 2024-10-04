13c9e63d507a41a37a3a5700a04a99f2dfaf6c7a
Kevin Müller <mkevin2006@gmail.com>
2024-10-04 20:02:08 +0200

    Have Instrument take Tone as argument

--- src/instrument.rs
+++ src/instrument.rs
@@ -1,16 +1,9 @@
 pub mod predefined;
 
-use std::time::Duration;
-
-#[derive(Clone, Copy)]
-pub struct ToneInfo<T> {
-    pub tone: T,
-    pub time: Duration,
-    pub intensity: f32,
-}
+use crate::file_export::export_info::Tone;
 
 pub trait Instrument: Clone {
     type ConcreteValue: Clone + Copy;
 
-    fn generate_sound(&self, info: ToneInfo<Self::ConcreteValue>) -> f32;
+    fn generate_sound(&self, info: Tone<Self::ConcreteValue>) -> f32;
 }
