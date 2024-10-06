0d2eef5432119322f47be7495edaf921044d679e
Kevin Müller <mkevin2006@gmail.com>
2024-10-06 16:24:47 +0200

    Adjust predefined instruments

--- src/instrument/predefined.rs
+++ src/instrument/predefined.rs
@@ -3,8 +3,7 @@ pub mod tet12;
 use tet12::TET12ConcreteTone;
 use super::Instrument;
 use super::Tone;
-
-/*
+use crate::file_export::export_info::SoundBuffer;
 
 use std::time::Duration;
 
@@ -15,6 +14,17 @@ struct SineGenerator;
 struct TriangleGenerator;
 
 impl SineGenerator {
+    pub fn generate(info: &Tone<tet12::TET12ConcreteTone>, time: Duration) -> f32 {
+        let mut result = 0.0;
+
+        for tone in &info.concrete_values {
+            let frequency = tone.to_frequency() as f64;
+            result += Self::wave(frequency, time);
+        }
+
+        return result * info.intensity.start;
+    }
+
     fn wave(frequency: f64, time: Duration) -> f32 {
         use std::f64::consts::PI;
         (time.as_secs_f64() * frequency * 2.0 * PI).sin() as f32
@@ -22,6 +32,17 @@ impl SineGenerator {
 }
 
 impl TriangleGenerator {
+    pub fn generate(info: &Tone<tet12::TET12ConcreteTone>, time: Duration) -> f32 {
+        let mut result = 0.0;
+
+        for tone in &info.concrete_values {
+            let frequency = tone.to_frequency() as f64;
+            result += Self::wave(frequency, time);
+        }
+
+        return result;
+    }
+
     fn wave(frequency: f64, time: Duration) -> f32 {
         use std::f64::consts::PI;
         let x = time.as_secs_f64() * frequency * 2.0 * PI;
@@ -32,30 +53,21 @@ impl TriangleGenerator {
 impl Instrument for SineGenerator {
     type ConcreteValue = TET12ConcreteTone;
 
-    fn generate_sound(&self, info: &Tone<Self::ConcreteValue>, time: std::time::Duration) -> f32 {
-        let mut result = 0.0;
-
-        for tone in &info.concrete_values {
-            let frequency = tone.to_frequency() as f64;
-            result += Self::wave(frequency, time);
+    fn generate_sound(&self, buffer: &mut SoundBuffer, info: &Tone<Self::ConcreteValue>) {
+        for i in 0..buffer.samples.len() {
+            let time = buffer.get_time_from_index(i);
+            buffer.samples[i] = Self::generate(info, time);
         }
-
-        return result * info.intensity.start;
     }
 }
 
 impl Instrument for TriangleGenerator {
     type ConcreteValue = TET12ConcreteTone;
 
-    fn generate_sound(&self, info: &Tone<Self::ConcreteValue>, time: std::time::Duration) -> f32 {
-        let mut result = 0.0;
-
-        for tone in &info.concrete_values {
-            let frequency = tone.to_frequency() as f64;
-            result += Self::wave(frequency, time);
+    fn generate_sound(&self, buffer: &mut SoundBuffer, info: &Tone<Self::ConcreteValue>) {
+        for i in 0..buffer.samples.len() {
+            let time = buffer.get_time_from_index(i);
+            buffer.samples[i] = Self::generate(info, time);
         }
-
-        return result * info.intensity.start;
     }
 }
-*/
