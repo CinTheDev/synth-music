cecfc060ea3c356cb28d381f1504f4ea2f5f5471
Kevin Müller <mkevin2006@gmail.com>
2024-10-06 18:42:17 +0200

    Write more unit tests for mix()

--- src/file_export/export_info.rs
+++ src/file_export/export_info.rs
@@ -117,6 +117,132 @@ impl SoundBuffer {
 mod tests {
     use super::*;
 
+    // Tests for mix()
+
+    fn assert_soundbuffer_equal(a: SoundBuffer, b: SoundBuffer) {
+        assert_eq!(a.sample_rate, b.sample_rate);
+        assert_eq!(a.active_samples, b.active_samples);
+        assert_eq!(a.samples, b.samples);
+    }
+    
+    #[test]
+    fn soundbuffer_mix_simple() {
+        let first_buffer = SoundBuffer::new(
+            vec![0.1, 0.2, 0.3],
+            44100,
+            3,
+        );
+        let second_buffer = SoundBuffer::new(
+            vec![0.4, 0.2, 0.0],
+            44100,
+            3,
+        );
+
+        let result = first_buffer.mix(second_buffer);
+
+        let expected = SoundBuffer::new(
+            vec![
+                0.1 + 0.4,
+                0.2 + 0.2,
+                0.3 + 0.0,
+            ],
+            44100,
+            3
+        );
+
+        assert_soundbuffer_equal(result, expected);
+    }
+
+    #[test]
+    fn soundbuffer_mix_partial_full() {
+        let first_buffer = SoundBuffer::new(
+            vec![0.1, 0.2, 0.3, 0.4],
+            44100,
+            4,
+        );
+        let second_buffer = SoundBuffer::new(
+            vec![0.4, 0.2, 0.0],
+            44100,
+            3,
+        );
+
+        let result = first_buffer.mix(second_buffer);
+
+        let expected = SoundBuffer::new(
+            vec![
+                0.1 + 0.4,
+                0.2 + 0.2,
+                0.3 + 0.0,
+                0.4,
+            ],
+            44100,
+            4
+        );
+
+        assert_soundbuffer_equal(result, expected);
+    }
+
+    #[test]
+    fn soundbuffer_mix_partial_half_full() {
+        let first_buffer = SoundBuffer::new(
+            vec![0.1, 0.2, 0.3, 0.4],
+            44100,
+            3,
+        );
+        let second_buffer = SoundBuffer::new(
+            vec![0.4, 0.2, 0.0],
+            44100,
+            3,
+        );
+
+        let result = first_buffer.mix(second_buffer);
+
+        let expected = SoundBuffer::new(
+            vec![
+                0.1 + 0.4,
+                0.2 + 0.2,
+                0.3 + 0.0,
+                0.4
+            ],
+            44100,
+            3
+        );
+
+        assert_soundbuffer_equal(result, expected);
+    }
+
+    #[test]
+    fn soundbuffer_mix_partial_not_full() {
+        let first_buffer = SoundBuffer::new(
+            vec![0.1, 0.2, 0.3, 0.4, 0.5],
+            44100,
+            2,
+        );
+        let second_buffer = SoundBuffer::new(
+            vec![0.4, 0.2, 0.0, -0.3],
+            44100,
+            3,
+        );
+
+        let result = first_buffer.mix(second_buffer);
+
+        let expected = SoundBuffer::new(
+            vec![
+                0.1 + 0.4,
+                0.2 + 0.2,
+                0.3 + 0.0,
+                0.4 - 0.3,
+                0.5,
+            ],
+            44100,
+            3
+        );
+
+        assert_soundbuffer_equal(result, expected);
+    }
+
+    // Tests for append()
+    
     #[test]
     fn soundbuffer_append_simple() {
         let mut first_buffer = SoundBuffer::new(
