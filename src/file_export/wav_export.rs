767a8f09c0cecf4fe4f4b604c2b90124d6970b8b
Kevin Müller <mkevin2006@gmail.com>
2024-10-08 20:07:31 +0200

    Remove bits_per_sample field of WavExport

--- src/file_export/wav_export.rs
+++ src/file_export/wav_export.rs
@@ -7,7 +7,6 @@ use std::str::FromStr;
 
 pub struct WavExport {
     pub path: PathBuf,
-    pub bits_per_sample: u16,
 }
 
 impl WavExport {
@@ -25,6 +24,8 @@ impl WavExport {
         const DATA: [u8; 4] = [b'd', b'a', b't', b'a'];
 
         let sample_rate = settings.sample_rate;
+        // TODO: Make this configurable
+        let bits_per_sample = 16;
 
         let data_size: u32 = buffer_size.try_into().unwrap();
         let file_size = data_size + 44 - 8;
@@ -35,10 +36,10 @@ impl WavExport {
         let num_channels: u16 = 1;
 
         let sample_rate_calculation: u32 =
-            sample_rate * self.bits_per_sample as u32 * num_channels as u32 / 8;
+            sample_rate * bits_per_sample as u32 * num_channels as u32 / 8;
         
         let bits_sample_calculation: u16 =
-            self.bits_per_sample * num_channels / 8;
+            bits_per_sample * num_channels / 8;
 
         writer.write(&RIFF)?;
         writer.write(bytes_of(&file_size))?;
@@ -50,7 +51,7 @@ impl WavExport {
         writer.write(bytes_of(&sample_rate))?;
         writer.write(bytes_of(&sample_rate_calculation))?;
         writer.write(bytes_of(&bits_sample_calculation))?;
-        writer.write(bytes_of(&self.bits_per_sample))?;
+        writer.write(bytes_of(&bits_per_sample))?;
         writer.write(&DATA)?;
         writer.write(bytes_of(&data_size))?;
 
@@ -62,9 +63,6 @@ impl FileExport for WavExport {
     fn export(&self, buffer: super::SoundBuffer) -> std::io::Result<()> {
         let f = File::create(&self.path)?;
         let mut writer = BufWriter::new(f);
-
-        // TODO: Generate chunks
-        //let samples = buffer.generate_whole_buffer(self.sample_rate);
 
         self.write_header(&mut writer, buffer.settings(), buffer.samples.len() * 2)?;
         let amplitude = 0xFFFF as f32 * 0.1;
@@ -82,7 +80,6 @@ impl Default for WavExport {
     fn default() -> Self {
         Self {
             path: PathBuf::from_str("unnamed.wav").unwrap(),
-            bits_per_sample: 16,
         }
     }
 }
