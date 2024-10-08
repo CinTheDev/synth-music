3f9be0531abc1e021db5a9fd9d599740bf1d3af6
Kevin Müller <mkevin2006@gmail.com>
2024-10-08 21:57:21 +0200

    Add square wave to predefined instruments

--- src/instrument/predefined.rs
+++ src/instrument/predefined.rs
@@ -13,6 +13,9 @@ struct SineGenerator;
 #[derive(Clone, Copy)]
 struct TriangleGenerator;
 
+#[derive(Clone, Copy)]
+struct SquareGenerator;
+
 impl SineGenerator {
     pub fn generate(tones: &Tone<tet12::TET12ConcreteTone>, time: Duration) -> f32 {
         let mut result = 0.0;
@@ -50,6 +53,24 @@ impl TriangleGenerator {
     }
 }
 
+impl SquareGenerator {
+    pub fn generate(tones: &Tone<tet12::TET12ConcreteTone>, time: Duration) -> f32 {
+        let mut result = 0.0;
+
+        for tone in &tones.concrete_values {
+            let frequency = tone.to_frequency() as f64;
+            result += Self::wave(frequency, time);
+        }
+
+        return result * tones.intensity.start;
+    }
+
+    fn wave(frequency: f64, time: Duration) -> f32 {
+        let x = (2.0 * time.as_secs_f64() * frequency + 1.0).floor() as u32;
+        return 2.0 * (x % 2) as f32 - 1.0;
+    }
+}
+
 impl Instrument for SineGenerator {
     type ConcreteValue = TET12ConcreteTone;
 
@@ -69,6 +90,21 @@ impl Instrument for TriangleGenerator {
     type ConcreteValue = TET12ConcreteTone;
 
     fn render_buffer(&self, buffer_info: super::BufferInfo, tones: &Tone<Self::ConcreteValue>) -> super::InstrumentBuffer {
+        let mut buffer = Vec::new();
+
+        for i in 0..buffer_info.tone_samples {
+            let time = buffer_info.time_from_index(i);
+            buffer.push(Self::generate(tones, time));
+        }
+
+        InstrumentBuffer { samples: buffer }
+    }
+}
+
+impl Instrument for SquareGenerator {
+    type ConcreteValue = TET12ConcreteTone;
+
+    fn render_buffer(&self, buffer_info: super::BufferInfo, tones: &Tone<Self::ConcreteValue>) -> InstrumentBuffer {
         let mut buffer = Vec::new();
 
         for i in 0..buffer_info.tone_samples {
