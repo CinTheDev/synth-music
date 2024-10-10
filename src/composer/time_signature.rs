603c2ad14aaf8538f848dff6714804c7550cc8de
Kevin Müller <mkevin2006@gmail.com>
2024-10-10 19:27:36 +0200

    Implement new for TimeSignature

--- src/composer/time_signature.rs
+++ src/composer/time_signature.rs
@@ -7,7 +7,36 @@ pub struct TimeSignature {
 }
 
 impl TimeSignature {
+    pub fn new(nominator: u8, denominator: u8) -> Self {
+        // For now only powers of two for denominator are supported
+        if ! denominator.is_power_of_two() {
+            panic!("The denominator can only be a power of two.");
+        }
+
+        let subdivision = Self::what_power_of_two(denominator) - 1;
+        let measure_length =
+            Length::from_subdivisions(subdivision.into())
+            * nominator.into();
+
+        Self {
+            measure_length,
+        }
+    }
+
     pub fn is_measure_saturated(&self, lengths: Length) -> bool {
         return self.measure_length == lengths;
+    }
+
+    fn what_power_of_two(mut value: u8) -> u8 {
+        let mut result = 0;
+
+        if value == 0  { panic!("Invalid input") };
+
+        while value > 1 {
+            value >>= 1;
+            result += 1;
+        }
+
+        return result;
     }
 }
