b157811ef7d53e216883f9c51772313f5cd1721e
Kevin Müller <mkevin2006@gmail.com>
2024-10-10 18:28:40 +0200

    Change smallest tick to 65536

--- src/composer/note/length.rs
+++ src/composer/note/length.rs
@@ -1,23 +1,30 @@
 
-pub const WHOLE:     Length = Length::new(256*256);
-pub const HALF:      Length = Length::new(256*256 / 2);
-pub const QUARTER:   Length = Length::new(256*256 / 4);
-pub const EIGTH:     Length = Length::new(256*256 / 8);
-pub const SIXTEENTH: Length = Length::new(256*256 / 16);
+pub const WHOLE:     Length = Length::from_subdivisions(0);
+pub const HALF:      Length = Length::from_subdivisions(1);
+pub const QUARTER:   Length = Length::from_subdivisions(2);
+pub const EIGTH:     Length = Length::from_subdivisions(3);
+pub const SIXTEENTH: Length = Length::from_subdivisions(4);
+
+const TICKS_WHOLE: u32 = 2_u32.pow(16);
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Length {
-    // Number of 256th values
+    // Number of 65536th values
     ticks: u32,
     ntole_index: u8,
 }
 
 impl Length {
-    pub const fn new(ticks: u32) -> Self {
+    pub const fn from_ticks(ticks: u32) -> Self {
         Self {
             ticks,
             ntole_index: 0,
         }
+    }
+
+    pub const fn from_subdivisions(subdivision: u32) -> Self {
+        let ticks = TICKS_WHOLE / 2_u32.pow(subdivision);
+        Self::from_ticks(ticks)
     }
 
     pub const fn dot(mut self) -> Self {
@@ -55,6 +62,6 @@ impl Length {
     }
 
     pub fn to_float(&self) -> f32 {
-        self.ticks as f32 / 256.0
+        self.ticks as f32 / TICKS_WHOLE as f32
     }
 }
