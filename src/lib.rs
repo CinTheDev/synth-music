a79e959831752f37a3a99299d38c8ae8bdf84a68
Kevin Müller <mkevin2006@gmail.com>
2024-10-11 20:42:36 +0200

    Document terms

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+/*!
+A framework-like crate to compose and synthetisize music. It's possible to work
+without external tools just using this crate.
+
+# General principles
+
+Most important key information on how this crate is structured.
+
+## Terms
+
+A whole music piece that would usually reside in a dedicated sound file is
+called a **composition**.
+
+A composition is made of several **sections** that are put behind after one
+another. A section has a defined speed (BPM) and Music Key. These parameters
+can change with every new section.
+
+A section contains several **tracks**. Unlike sections, all tracks of a section
+are played at the same time. The track stores the individual notes and is
+assigned an **instrument**.
+
+An instrument is a user-defined implementation to generate sound given a note or
+frequency. Here the sound synthesis happens. There are predefined instruments
+included, but it's encouraged to implement own instruments.
+
+*/
+
 pub mod composer;
 pub mod instrument;
 pub mod file_export;
