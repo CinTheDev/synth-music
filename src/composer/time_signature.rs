7a7f14c9667ace5fc9d7542786faa7cfdf737e09
Kevin Müller <mkevin2006@gmail.com>
2024-10-11 14:38:46 +0200

    Implement beat_length and beat_intensities calculation

--- src/composer/time_signature.rs
+++ src/composer/time_signature.rs
@@ -1,33 +1,37 @@
 use super::note::Length;
 
-pub const FOUR_FOUR: TimeSignature = TimeSignature::new(4, 4);
-pub const THREE_FOUR: TimeSignature = TimeSignature::new(3, 4);
-pub const TWO_FOUR: TimeSignature = TimeSignature::new(2, 4);
+//pub const FOUR_FOUR: TimeSignature = TimeSignature::new(4, 4);
+//pub const THREE_FOUR: TimeSignature = TimeSignature::new(3, 4);
+//pub const TWO_FOUR: TimeSignature = TimeSignature::new(2, 4);
 
-pub const SIX_EIGHT: TimeSignature = TimeSignature::new(6, 8);
-pub const NINE_EIGHT: TimeSignature = TimeSignature::new(9, 8);
-pub const TWELVE_EIGHT: TimeSignature = TimeSignature::new(12, 8);
+//pub const SIX_EIGHT: TimeSignature = TimeSignature::new(6, 8);
+//pub const NINE_EIGHT: TimeSignature = TimeSignature::new(9, 8);
+//pub const TWELVE_EIGHT: TimeSignature = TimeSignature::new(12, 8);
 
 #[derive(Clone)]
 pub struct TimeSignature {
     pub measure_length: Length,
-    // TODO: Beats
+    beat_length: Length,
+    beat_intensities: Vec<f32>,
 }
 
 impl TimeSignature {
-    pub const fn new(nominator: u8, denominator: u8) -> Self {
+    pub fn new(nominator: u8, denominator: u8) -> Self {
         // For now only powers of two for denominator are supported
         if ! denominator.is_power_of_two() {
             panic!("The denominator can only be a power of two.");
         }
 
         let subdivision = Self::what_power_of_two(denominator);
-        let measure_length =
-            Length::from_subdivisions(subdivision as u32)
-            .const_mul(nominator as u32);
+
+        let beat_length = Length::from_subdivisions(subdivision.into());
+        let measure_length = beat_length * nominator.into();
+        let beat_intensities = vec![1.0; nominator.into()];
 
         Self {
             measure_length,
+            beat_length,
+            beat_intensities,
         }
     }
 
@@ -35,7 +39,7 @@ impl TimeSignature {
         return self.measure_length == lengths;
     }
 
-    const fn what_power_of_two(mut value: u8) -> u8 {
+    fn what_power_of_two(mut value: u8) -> u8 {
         let mut result = 0;
 
         if value == 0  { panic!("Invalid input") };
