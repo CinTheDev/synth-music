e600a3703632314f06d40a1e5e6fc0f42b9eefa2
Kevin Müller <mkevin2006@gmail.com>
2024-10-12 14:44:14 +0200

    Document MusicKey

--- src/composer/music_key.rs
+++ src/composer/music_key.rs
@@ -1,3 +1,4 @@
+/// The tonic of a music key
 #[derive(Clone, Copy)]
 pub enum KeyTonic {
     B,
@@ -24,11 +25,24 @@ pub enum KeyTonic {
     Csharp,
     C,
 }
+
+/// The type of a music key
+/// 
+/// In music theory, this is usually inferred from the melody. In this crate
+/// it does affect the notes, so it must be specified.
 #[derive(Clone, Copy)]
 pub enum KeyType {
     Major,
     Minor,
 }
+
+/// Represents a key in music theory.
+/// 
+/// It is used to convert `ScaledValue` into `ConcreteValue`. Currently this
+/// only makes sense for the 12-TET note system, or partially for other similar
+/// systems.
+/// 
+/// `MusicKey` also stores whether the key is Major or Minor.
 #[derive(Clone, Copy)]
 pub struct MusicKey {
     pub tonic: KeyTonic,
