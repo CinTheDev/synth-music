2980ef927b1600f7b195581e18a7fae00e6b6052
Kevin Müller <mkevin2006@gmail.com>
2024-10-13 19:39:55 +0200

    Write test for to_float

--- src/composer/note/length/tests.rs
+++ src/composer/note/length/tests.rs
@@ -42,3 +42,36 @@ fn test_multidot() {
     assert_eq!(whole_3dot, WHOLE + HALF + QUARTER);
     assert_eq!(whole4_4dot, WHOLE*4 + WHOLE*2 + WHOLE + HALF);
 }
+
+#[test]
+fn test_tofloat() {
+    let whole = WHOLE.to_float();
+    let quarter = QUARTER.to_float();
+
+    let quarter_dot = QUARTER.dot().to_float();
+    let half_2dot = HALF.multi_dot(2).to_float();
+
+    let quarter_triole = QUARTER.triole().to_float();
+    let half_pentole = HALF.ntole(5).to_float();
+
+    let whole2_2dot = (WHOLE * 2).multi_dot(2).to_float();
+
+    let epsilon = 0.001;
+
+    assert_eq_f32(whole, 1.0, epsilon);
+    assert_eq_f32(quarter, 0.25, epsilon);
+    assert_eq_f32(quarter_dot, 0.25 + 0.125, epsilon);
+    assert_eq_f32(half_2dot, 0.5 + 0.25 + 0.125, epsilon);
+    assert_eq_f32(quarter_triole, 0.5 / 3.0, epsilon);
+    assert_eq_f32(half_pentole, 1.0 / 5.0, epsilon);
+    assert_eq_f32(whole2_2dot, 2.0 + 1.0 + 0.5, epsilon);
+}
+
+// Utility functions
+
+fn assert_eq_f32(a: f32, b: f32, epsilon: f32) {
+    let delta = (a - b).abs();
+    if delta > epsilon {
+        panic!("assertion failed: {} != {}", a, b);
+    }
+}
