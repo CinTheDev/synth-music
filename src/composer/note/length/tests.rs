496a4b65474edb8d7dbee7d1eeee1965b3e7dc0f
Kevin Müller <mkevin2006@gmail.com>
2024-10-13 19:31:18 +0200

    Write test for dot function

--- src/composer/note/length/tests.rs
+++ src/composer/note/length/tests.rs
@@ -14,3 +14,17 @@ fn test_construction_subdivisions() {
     assert_eq!(quarter,  Length::from_ticks(2_u32.pow(14)));
     assert_eq!(smallest, Length::from_ticks(1));
 }
+
+#[test]
+fn test_dot() {
+    let quarter_dot = QUARTER.dot();
+    let sixteenth_dot = SIXTEENTH.dot();
+    
+    let whole_dot = WHOLE.dot();
+    let whole4_dot = (WHOLE * 4).dot();
+
+    assert_eq!(quarter_dot, QUARTER + EIGTH);
+    assert_eq!(sixteenth_dot, SIXTEENTH + Length::from_subdivisions(5));
+    assert_eq!(whole_dot, WHOLE + HALF);
+    assert_eq!(whole4_dot, WHOLE * 6);
+}
