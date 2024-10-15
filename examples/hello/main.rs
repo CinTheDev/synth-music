939d460f93d4c192234bff32d693a06360d2daf8
Kevin Müller <mkevin2006@gmail.com>
2024-10-15 19:51:41 +0200

    Implement the export process in hello example

--- examples/hello/main.rs
+++ examples/hello/main.rs
@@ -3,7 +3,37 @@ use synth_music::prelude::*;
 fn main() {
     println!("Hello example");
 
+    // Create implemented instrument and tracks
     let example_instrument = SineInstrument;
+    let track = create_track(example_instrument);
+
+    // Specify settings (must be constant throughout the whole composition)
+    let settings = CompositionSettings {
+        sample_rate: 44100,
+    };
+
+    // Specify attributes (can change if section transition occurs)
+    let info = SectionInfo {
+        bpm: 120.0,
+        key: music_key::C_MAJOR,
+        settings: &settings,
+    };
+
+    // Render the section, it's possible to enter any number of tracks.
+    // All tracks will play at the same time, parallel to one another.
+    let section = section!(info,
+        track,
+    );
+
+    // Append all sections together, any number of sections is accepted.
+    // In this example the given section is repeated.
+    let composition = composition!(
+        section,
+        section
+    );
+
+    // Export the composition. Check the function implementation below.
+    export(composition);
 }
 
 fn create_track<T>(instrument: T) -> MeasureTrack<TET12ScaledTone, T>
@@ -89,18 +119,20 @@ where
     return track;
 }
 
-fn export(buffer: SoundBuffer, name: &str) {
+fn export(buffer: SoundBuffer) {
     use std::path::PathBuf;
 
     if std::fs::read_dir("export").is_err() {
         std::fs::create_dir("export").unwrap();
     }
 
+    // Specify new exporter with given attributes
     let wav_export = WavExport {
-        path: PathBuf::from("export").join(name),
+        path: PathBuf::from("export/Hello.wav"),
         ..Default::default()
     };
 
+    // Actually export the piece.
     wav_export.export(buffer).unwrap();
 }
 
