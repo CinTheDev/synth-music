55d345a603a6d17ecadca7845112ace518c539b3
Kevin Müller <mkevin2006@gmail.com>
2024-10-15 20:26:46 +0200

    Create new example for time signatures

---
+++ examples/time_signatures/main.rs
@@ -0,0 +1,32 @@
+use synth_music::prelude::*;
+
+fn main() {
+
+}
+
+fn track_four_four() -> MeasureTrack<TET12ScaledTone, predefined::TriangleGenerator> {
+    use tet12::*;
+    use length::*;
+
+    let time_signature =
+        TimeSignature::new(4, 4)
+        .set_beat(0, 1.1)
+        .set_beat(2 , 1.05);
+
+    let mut track = MeasureTrack::new(predefined::TriangleGenerator, time_signature);
+
+    for _ in 0..4 {
+        sequential_notes!(track, QUARTER,
+            first(3),
+            first(3),
+            first(3),
+            first(3),
+        );
+        track.measure().unwrap();
+    }
+
+    track.pause(WHOLE);
+    track.measure().unwrap();
+
+    return track;
+}
