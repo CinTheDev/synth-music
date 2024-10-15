8d6491981f38de85b948fc0062dbe1c9d2d60929
Kevin Müller <mkevin2006@gmail.com>
2024-10-15 20:57:40 +0200

    Comment note system definitions

--- examples/custom_note_system/drumset.rs
+++ examples/custom_note_system/drumset.rs
@@ -1,5 +1,6 @@
 use synth_music::prelude::*;
 
+// Specify possible drumset actions
 #[derive(Clone, Copy)]
 pub enum DrumsetAction {
     Bass,
@@ -7,9 +8,15 @@ pub enum DrumsetAction {
     HiHat,
 }
 
+// !! This is important
+// This will convert the ScaledValue into a ConcreteValue, which is then
+// given to the instrument. It's also possible to not perform any conversion
+// at all and to just return a copy of self. (But this still needs to be
+// implemented to be placed on tracks.)
 impl ScaledValue for DrumsetAction {
     type ConcreteValue = Self;
 
+    // The music key is availabe for conversion, but in this example it is not needed.
     fn to_concrete_value(&self, _key: MusicKey) -> Self::ConcreteValue {
         self.to_owned()
     }
