dbed51183cc1a89baba32c33e44b00dc5ef011ae
Kevin Müller <mkevin2006@gmail.com>
2024-10-15 20:27:43 +0200

    Implement three four beat

--- examples/time_signatures/main.rs
+++ examples/time_signatures/main.rs
@@ -30,3 +30,28 @@ fn track_four_four() -> MeasureTrack<TET12ScaledTone, predefined::TriangleGenera
 
     return track;
 }
+
+fn track_three_four() -> MeasureTrack<TET12ScaledTone, predefined::TriangleGenerator> {
+    use tet12::*;
+    use length::*;
+
+    let time_signature =
+        TimeSignature::new(3, 4)
+        .set_beat(0, 1.1);
+
+    let mut track = MeasureTrack::new(predefined::TriangleGenerator, time_signature);
+
+    for _ in 0..4 {
+        sequential_notes!(track, QUARTER,
+            third(3),
+            third(3),
+            third(3),
+        );
+        track.measure().unwrap();
+    }
+
+    track.pause(WHOLE);
+    track.measure().unwrap();
+
+    return track;
+}
