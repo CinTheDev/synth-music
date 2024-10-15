de4b9fa0803c5676df92f0a0a1bc30243d4ff391
Kevin Müller <mkevin2006@gmail.com>
2024-10-15 20:28:57 +0200

    Implement six eight beat

--- examples/time_signatures/main.rs
+++ examples/time_signatures/main.rs
@@ -55,3 +55,33 @@ fn track_three_four() -> MeasureTrack<TET12ScaledTone, predefined::TriangleGener
 
     return track;
 }
+
+fn track_six_eight() -> MeasureTrack<TET12ScaledTone, predefined::TriangleGenerator> {
+    use tet12::*;
+    use length::*;
+
+    let time_signature =
+        TimeSignature::new(6, 8)
+        .set_beat(0, 1.1)
+        .set_beat(3 , 1.05);
+
+    let mut track = MeasureTrack::new(predefined::TriangleGenerator, time_signature);
+
+    for _ in 0..4 {
+        sequential_notes!(track, QUARTER,
+            first(3),
+            first(3),
+            first(3),
+
+            third(3),
+            third(3),
+            third(3),
+        );
+        track.measure().unwrap();
+    }
+
+    track.pause(WHOLE);
+    track.measure().unwrap();
+
+    return track;
+}
