358fd63dce76fa38d448721f4425f4be762ad947
Kevin Müller <mkevin2006@gmail.com>
2024-10-15 21:07:12 +0200

    Create a new drumset instrument struct

--- examples/custom_note_system/drumset.rs
+++ examples/custom_note_system/drumset.rs
@@ -1,4 +1,5 @@
 use synth_music::prelude::*;
+use std::time::Duration;
 
 // Specify possible drumset actions
 #[derive(Clone, Copy)]
@@ -19,5 +20,37 @@ impl ScaledValue for DrumsetAction {
     // The music key is availabe for conversion, but in this example it is not needed.
     fn to_concrete_value(&self, _key: MusicKey) -> Self::ConcreteValue {
         self.to_owned()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Drumset {
+    play_duration: Duration
+}
+
+impl Drumset {
+    pub fn new() -> Self {
+        Self {
+            play_duration: Duration::from_secs_f32(0.3),
+        }
+    }
+
+    pub fn generate_white_noise(&self, buffer: &mut Vec<f32>) {
+
+    }
+}
+
+impl Instrument for Drumset {
+    type ConcreteValue = DrumsetAction;
+
+    fn render_buffer(&self, buffer_info: BufferInfo, tones: &Tone<Self::ConcreteValue>) -> InstrumentBuffer {
+        let target_samples = (self.play_duration.as_secs_f32() * buffer_info.sample_rate as f32)
+            .ceil() as usize;
+        
+        let mut buffer = vec![0_f32; target_samples];
+
+        self.generate_white_noise(&mut buffer);
+
+        InstrumentBuffer { samples: buffer }
     }
 }
