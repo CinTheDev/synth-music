cd683d963e6bf15659d79e870ee9901886fd23c3
Kevin Müller <mkevin2006@gmail.com>
2024-11-05 20:21:54 +0100

    Remove all unused functions

--- src/composer/measure_track.rs
+++ src/composer/measure_track.rs
@@ -1,15 +1,12 @@
 use super::UnboundTrack;
 
-use super::{Note, ScaledValue, length, Length};
+use super::{Note, ScaledValue, Length};
 use super::note::DynamicsFlag;
 
 use super::{TimeSignature, SectionInfo, MusicTrack};
-use super::{ExportTrack, Tone};
+use super::ExportTrack;
 
 use crate::instrument::Instrument;
-
-use std::time::Duration;
-use std::ops::Range;
 
 /// An implementation of MusicTrack with additional rules to ensure that
 /// Measures are filled with notes correctly. Use this as the standard Track.
@@ -147,163 +144,6 @@ where
 
         let active_measure = self.get_active_measure();
         return active_measure.notes.last_mut().unwrap();
-    }
-
-    fn arrange_notes(&self) -> Vec<(Note<T>, Length)> {
-        let mut notes = Vec::new();
-
-        let active_measure = self.active_measure.as_ref().unwrap();
-
-        if ! active_measure.is_empty() {
-            eprintln!("WARNING: Unvalidated measure, you probably forgot to call track.measure() at the end.");
-        }
-
-        for measure in &self.measures {
-            let mut note_lengths = Vec::new();
-
-            for note in &measure.notes {
-                let position_in_measure =
-                    Length::count_lengths(&note_lengths)
-                    .unwrap_or(length::INVALID);
-
-                notes.push(((*note).clone(), position_in_measure));
-                note_lengths.push(note.length);
-            }
-        }
-
-        return notes;
-    }
-
-    fn conversion_first_pass(&self, notes: &Vec<(Note<T>, Length)>, section_info: SectionInfo) -> Vec<Tone<U::ConcreteValue>> {
-        let mut tones = Vec::new();
-
-        for note in notes {
-            let tone = self.generate_tone(&note, section_info);
-            tones.push(tone);
-        }
-
-        return tones;
-    }
-
-    // WARNING: Assumes that notes align with tones
-    // Fix if this doesn't apply anymore
-    fn conversion_pass_dynamics(
-        notes: &Vec<(Note<T>, Length)>,
-        tones: &mut Vec<Tone<U::ConcreteValue>>
-    ) {
-        let mut i = 0;
-
-        while let Some(notes_range) = Self::find_next_dynamics_change(&notes, i) {
-            i = notes_range.end;
-            Self::calculate_dynamics_over_notes(tones, notes_range);
-        }
-    }
-
-    fn find_next_dynamics_change(notes: &Vec<(Note<T>, Length)>, start_index: usize) -> Option<Range<usize>> {
-        use super::note::DynamicsFlag;
-
-        let mut index_dynamics_start = None;
-
-        for i in start_index..notes.len() {
-            let (note, _) = &notes[i];
-
-            if note.dynamics_flag == DynamicsFlag::StartChange {
-                if index_dynamics_start.is_some() {
-                    panic!("Doubled StartChange.");
-                }
-                
-                index_dynamics_start = Some(i);
-            }
-            if note.dynamics_flag == DynamicsFlag::EndChange {
-                if let Some(index_dynamics_start) = index_dynamics_start {
-                    return Some(index_dynamics_start..(i + 1));
-                }
-
-                panic!("EndChange without preceding StartChange.");
-            }
-        }
-
-        if index_dynamics_start.is_some() {
-            panic!("StartChange without closing EndChange.");
-        }
-
-        return None;
-    }
-
-    fn calculate_dynamics_over_notes(tones: &mut Vec<Tone<U::ConcreteValue>>, range: Range<usize>) {
-        let start_intensity = tones[range.start].intensity.start;
-        let end_intensity = tones[range.end - 1].intensity.start;
-
-        let mut time_delta = Duration::ZERO;
-
-        for tone in &tones[range.clone()] {
-            time_delta += tone.play_duration;
-        }
-
-        // We'll assume linear intensity increase
-        // TODO: Make interpolation function custom
-
-        let time_delta = time_delta; // Make immutable
-        let mut current_time = Duration::ZERO;
-
-        for tone in &mut tones[range] {
-            let t = current_time.as_secs_f32() / time_delta.as_secs_f32();
-            let intensity_at_start = Self::interpolate_intensity(start_intensity, end_intensity, t);
-            
-            current_time += tone.play_duration;
-            let t = current_time.as_secs_f32() / time_delta.as_secs_f32();
-            let intensity_at_end = Self::interpolate_intensity(start_intensity, end_intensity, t);
-
-            tone.intensity = intensity_at_start..intensity_at_end;
-        }
-    }
-
-    fn interpolate_intensity(a: f32, b: f32, t: f32) -> f32 {
-        a + (b - a) * t
-    }
-
-    fn generate_tone(&self, note: &(Note<T>, Length), section_info: SectionInfo) -> Tone<U::ConcreteValue> {
-        let mut concrete_values = Vec::new();
-
-        let (note, position_in_measure) = note;
-
-        for scaled_value in &note.values {
-            let concrete_value = scaled_value.to_concrete_value(section_info.key);
-            concrete_values.push(concrete_value);
-        }
-
-        let play_duration = note.get_duration(section_info.bpm);
-        let tone_duration = play_duration.mul_f32(note.play_fraction);
-
-        let beat_emphasis = self.get_beat_from_position(*position_in_measure);
-
-        Tone {
-            concrete_values,
-            play_duration,
-            tone_duration,
-            intensity: note.intensity..note.intensity,
-            beat_emphasis,
-        }
-    }
-
-    fn get_beat_from_position(&self, position: Length) -> Option<f32> {
-        let beats = self.time_signature.beats();
-
-        let mut position_in_measure = length::ZERO;
-
-        for beat in beats {
-            if position == position_in_measure {
-                return Some(*beat);
-            }
-            // Missed the beat
-            if position.to_float() < position_in_measure.to_float() {
-                return None;
-            }
-
-            position_in_measure += self.time_signature.beat_length();
-        }
-
-        return None;
     }
 }
 
