bdb1af1c541d16be8a789c5d16908f390162b1e2
Kevin Müller <mkevin2006@gmail.com>
2024-11-17 13:04:09 +0100

    Implement noise_test

--- examples/noise_test/main.rs
+++ examples/noise_test/main.rs
@@ -1,7 +1,49 @@
 use synth_music::prelude::*;
 
 fn main() {
+    let settings = CompositionSettings {
+        sample_rate: 48000,
+    };
 
+    let sample_num = settings.sample_rate as usize;
+
+    let empty_buffer = SoundBuffer::from_parts(
+        vec![0.0; sample_num],
+        sample_num,
+        settings,
+    );
+
+    let silence_inbetween = SoundBuffer::from_parts(
+        vec![0.0; sample_num / 2],
+        sample_num / 2,
+        settings,
+    );
+
+    let mut white_noise = empty_buffer.clone();
+    let mut pink_noise = empty_buffer.clone();
+    let mut red_noise = empty_buffer.clone();
+    let mut blue_noise = empty_buffer.clone();
+    let mut purple_noise = empty_buffer.clone();
+
+    noise::white_noise(&mut white_noise.samples);
+    noise::pink_noise(&mut pink_noise);
+    noise::red_noise(&mut red_noise);
+    noise::blue_noise(&mut blue_noise);
+    noise::purple_noise(&mut purple_noise);
+
+    let out_buffer = composition!(
+        white_noise,
+        silence_inbetween,
+        pink_noise,
+        silence_inbetween,
+        red_noise,
+        silence_inbetween,
+        blue_noise,
+        silence_inbetween,
+        purple_noise,
+    );
+
+    export(out_buffer);
 }
 
 fn export(buffer: SoundBuffer) {
@@ -15,6 +57,6 @@ fn export(buffer: SoundBuffer) {
         path: PathBuf::from("export/Noise_Test.wav"),
         ..Default::default()
     };
-    
+
     wav_export.export(buffer).unwrap();
 }
