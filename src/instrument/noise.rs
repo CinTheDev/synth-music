89a5c59131101b80c3b0f7cf68d51d6997dae0b1
Kevin Müller <mkevin2006@gmail.com>
2024-11-17 12:04:35 +0100

    Document white noise function

--- src/instrument/noise.rs
+++ src/instrument/noise.rs
@@ -1,5 +1,7 @@
 use rand::Rng;
 
+/// Fill a given buffer with white noise. The samples are randomly generated
+/// in a range from -1.0 to 1.0.
 pub fn white_noise(buffer: &mut Vec<f32>) {
     let mut rng = rand::thread_rng();
 
