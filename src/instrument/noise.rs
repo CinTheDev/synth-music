b7f3318eb805e973454a8866b98b9fe7b06dad1b
Kevin Müller <mkevin2006@gmail.com>
2024-11-17 12:20:21 +0100

    Define blue and purple noise functions and fix descriptions

--- src/instrument/noise.rs
+++ src/instrument/noise.rs
@@ -11,9 +11,9 @@ pub fn white_noise(buffer: &mut Vec<f32>) {
 }
 
 /// Fill a given buffer with pink noise. In pink noise, the amplitude of a
-/// specific frequency is proportional to the square root of the frequency,
-/// which effectively makes higher frequencies quieter; it's similar to red
-/// noise. This makes it sound less harsh.
+/// specific frequency is inversly proportional to the square root of the
+/// frequency, which effectively makes higher frequencies quieter; it's similar
+/// to red noise. This makes it sound less harsh.
 /// 
 /// **Not implemented yet**
 pub fn pink_noise(buffer: &mut Vec<f32>) {
@@ -21,11 +21,30 @@ pub fn pink_noise(buffer: &mut Vec<f32>) {
 }
 
 /// Fill a given buffer with red noise. In red noise, the amplitude of a
-/// specific frequency is proportional to the frequency, which effectively makes
-/// higher frequencies quieter; it's similar to pink noise. This makes it sound
-/// less harsh.
+/// specific frequency is inversly proportional to the frequency, which
+/// effectively makes higher frequencies quieter; it's similar to pink noise.
+/// This makes it sound less harsh.
 /// 
 /// **Not implemented yet**
 pub fn red_noise(buffer: &mut Vec<f32>) {
+    unimplemented!();
+}
+
+/// Fill a given buffer with blue noise. In blue noise, the amplitude of a
+/// specific frequency is proportional to the square root of the frequency,
+/// which effectively makes lower frequencies quieter; it's similar to purple
+/// noise.
+/// 
+/// **Not implemented yet**
+pub fn blue_noise(buffer: &mut Vec<f32>) {
+    unimplemented!();
+}
+
+/// Fill a given buffer with purple noise. In purple noise, the amplitude of
+/// a specific frequency is proportional to the freqeuncy, which effectively
+/// makes lower frequencies quieter; it's similar to blue noise.
+/// 
+/// **Not implemented yet**
+pub fn purple_noise(buffer: &mut Vec<f32>) {
     unimplemented!();
 }
