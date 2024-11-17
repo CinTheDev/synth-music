6a45c00e7bb68e26fccb2f7600c4a88b54b3981e
Kevin Müller <mkevin2006@gmail.com>
2024-11-17 13:19:54 +0100

    Remove 'not implemented yet' for noise functions

--- src/instrument/noise.rs
+++ src/instrument/noise.rs
@@ -16,8 +16,6 @@ pub fn white_noise(buffer: &mut Vec<f32>) {
 /// specific frequency is inversly proportional to the square root of the
 /// frequency, which effectively makes higher frequencies quieter; it's similar
 /// to red noise. This makes it sound less harsh.
-/// 
-/// **Not implemented yet**
 pub fn pink_noise(buffer: &mut SoundBuffer) {
     let frequency_amplitude = |f: f32| {
         if f < 0.1 {
@@ -34,8 +32,6 @@ pub fn pink_noise(buffer: &mut SoundBuffer) {
 /// the amplitude of a specific frequency is inversly proportional to the
 /// frequency, which effectively makes higher frequencies quieter; it's similar
 /// to pink noise. This makes it sound less harsh.
-/// 
-/// **Not implemented yet**
 pub fn red_noise(buffer: &mut SoundBuffer) {
     let frequency_amplitude = |f: f32| {
         if f < 0.1 {
@@ -52,8 +48,6 @@ pub fn red_noise(buffer: &mut SoundBuffer) {
 /// specific frequency is proportional to the square root of the frequency,
 /// which effectively makes lower frequencies quieter; it's similar to purple
 /// noise.
-/// 
-/// **Not implemented yet**
 pub fn blue_noise(buffer: &mut SoundBuffer) {
     let frequency_amplitude = |f: f32| {
         let max_value = 20_000_f32.sqrt();
@@ -66,8 +60,6 @@ pub fn blue_noise(buffer: &mut SoundBuffer) {
 /// Fill a given buffer with purple noise. In purple noise, the amplitude of
 /// a specific frequency is proportional to the freqeuncy, which effectively
 /// makes lower frequencies quieter; it's similar to blue noise.
-/// 
-/// **Not implemented yet**
 pub fn purple_noise(buffer: &mut SoundBuffer) {
     let frequency_amplitude = |f: f32| {
         let max_value = 20_000.0;
@@ -86,8 +78,6 @@ pub fn purple_noise(buffer: &mut SoundBuffer) {
 /// the amplitude of the 1000Hz-portion of the noise will have 40% of the
 /// amplitude of what it would have in white noise. If every frequency has
 /// amplitude `1.0`. the white noise would remain unfiltered.
-/// 
-/// **Not implemented yet**
 pub fn custom_noise(buffer: &mut SoundBuffer, frequency_amplitude: fn(f32) -> f32) {
     white_noise(&mut buffer.samples);
     eq::filter_fft_whole(buffer, frequency_amplitude);
