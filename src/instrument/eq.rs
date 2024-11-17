1bc35f5181372da9aef0e15874ad5c97dbf584b6
Kevin Müller <mkevin2006@gmail.com>
2024-11-17 13:28:44 +0100

    Document fft filter

--- src/instrument/eq.rs
+++ src/instrument/eq.rs
@@ -1,6 +1,14 @@
 use super::SoundBuffer;
 use realfft::RealFftPlanner;
 
+/// Apply a FFT filter across the whole buffer, where the FFT length matches
+/// the buffer length. Only do this for periodic signals (e.g. tones).
+/// 
+/// This function will decompose the signal into its frequencies using a FFT,
+/// and will multiply them with numbers given by `frequency_amplitude`. This
+/// argument is a function that is given a frequency, and must return the
+/// desired amplitude for that frequency. At the end the signal is reconstructed
+/// using the altered frequencies.
 pub fn filter_fft_whole(buffer: &mut SoundBuffer, frequency_amplitude: fn(f32) -> f32) {
     let fft_len = buffer.samples.len();
 
