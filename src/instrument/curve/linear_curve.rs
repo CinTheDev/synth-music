7c1f88c8226e7ad007da8fd1a35e66c2a5c95937
Kevin Müller <mkevin2006@gmail.com>
2024-11-19 20:24:18 +0100

    Implement new() for LinearCurve

--- src/instrument/curve/linear_curve.rs
+++ src/instrument/curve/linear_curve.rs
@@ -1,7 +1,15 @@
 use super::Curve;
 
 pub struct LinearCurve {
+    points: Vec<(f32, f32)>,
+}
 
+impl LinearCurve {
+    pub fn new() -> Self {
+        Self {
+            points: Vec::new(),
+        }
+    }
 }
 
 impl Curve for LinearCurve {
