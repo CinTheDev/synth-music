ca34d973b9bf50bdf5ad1ebe126b8c215c9276d4
Kevin Müller <mkevin2006@gmail.com>
2024-11-20 12:11:42 +0100

    Document new Curve functionality

--- src/instrument/curve/linear_curve.rs
+++ src/instrument/curve/linear_curve.rs
@@ -1,5 +1,42 @@
 use super::{Curve, ScaleType};
 
+/// An implementor for `Curve`, the function is a set of connected points that
+/// interpolate linearly inbetween each other. The axis scale can be set to
+/// other types than linear for e.g. interpolating values across a logarithmic
+/// scale.
+/// 
+/// ```
+/// use synth_music::prelude::*;
+/// 
+/// # let settings = CompositionSettings {
+/// #     sample_rate: 48000,
+/// # };
+/// # let mut buffer = SoundBuffer::new(settings);
+/// 
+/// let curve = LinearCurve::new()
+///     .add_point(0.1, 0.0).unwrap()
+///     .add_point(0.3, 0.5).unwrap()
+///     .add_point(0.4, 1.0).unwrap()
+///     .add_point(0.5, 0.0).unwrap()
+///     .set_vertical_scale(ScaleType::Logarithmic);
+/// 
+/// eq::filter_fft(&mut buffer, curve.into_closure());
+/// ```
+/// 
+/// It's impossible to add new points inbetween already existing points. So
+/// newly placed points must always be to the right of all already placed
+/// points. If you do place it inbetween other points, `add_point` will return
+/// an error.
+/// 
+/// ```should_panic
+/// # use synth_music::prelude::*;
+/// 
+/// let curve = LinearCurve::new()
+///     .add_point(0.2, 0.0).unwrap()
+///     .add_point(0.5, 1.0).unwrap()
+///     .add_point(0.4, 0.8).unwrap()  // will panic!
+///     .add_point(0.8, 0.0).unwrap();
+/// ```
 pub struct LinearCurve {
     points: Vec<(f32, f32)>,
     horizontal_scale: ScaleType,
@@ -7,6 +44,8 @@ pub struct LinearCurve {
 }
 
 impl LinearCurve {
+    /// Create a new empty `LinearCurve` with the axes scale type set to
+    /// `Linear`.
     pub fn new() -> Self {
         Self {
             points: Vec::new(),
@@ -15,6 +54,8 @@ impl LinearCurve {
         }
     }
 
+    /// Add a point to the function. Will return an error if the resulting
+    /// function were to "go backwards".
     pub fn add_point(mut self, x: f32, y: f32) -> Result<Self, &'static str> {
         let last_point = self.points.last().unwrap_or(&(std::f32::NEG_INFINITY, 0.0));
 
@@ -26,11 +67,13 @@ impl LinearCurve {
         Ok(self)
     }
 
+    /// Set the scale type of the horizontal x-axis.
     pub fn set_horizontal_scale(mut self, scale_type: ScaleType) -> Self {
         self.horizontal_scale = scale_type;
         self
     }
 
+    /// Set the scale type of the vertical y-axis.
     pub fn set_vertical_scale(mut self, scale_type: ScaleType) -> Self {
         self.vertical_scale = scale_type;
         self
@@ -60,11 +103,5 @@ impl Curve for LinearCurve {
 
         let t = self.horizontal_scale.interpolate_inverse(left_point.0, right_point.0, x);
         return self.vertical_scale.interpolate(left_point.1, right_point.1, t);
-    }
-
-    fn into_closure(self) -> impl Fn(f32) -> f32 {
-        move |x| {
-            self.get(x)
-        }
     }
 }
